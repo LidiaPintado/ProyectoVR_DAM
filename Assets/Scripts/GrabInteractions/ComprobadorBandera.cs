b208909e69261e6d178dc2407da5dce1b3f56770
LidiaPintado <102482541+LidiaPintado@users.noreply.github.com>
2022-05-15 13:46:06 +0200

    ComprobadorBandera agregada logica para hacer outlines si se acierta
    ejemplo funcionando en escena tutorial

--- Assets/Scripts/GrabInteractions/ComprobadorBandera.cs
+++ Assets/Scripts/GrabInteractions/ComprobadorBandera.cs
@@ -9,39 +9,32 @@ public class ComprobadorBandera : MonoBehaviour
     [Tooltip("El XRGrabInteractable que deseas afectar o vacio para referenciar al objeto que lo contiene")]
     XRSocketInteractor socket;
 
-    /* Cuando una bandera entra en el socket comprobamos que sea la bandera correcta
-     * en caso de correcta (ALGO)
-     *      Outline Verde? ¿Permanente/Tiempo?
-     *      Sonido de exito?
-     * en caso de incorrecta ReturnHome
-     *      Sonido de error?
-     *      Outline rojo?-¿cuanto Tiempo?
-     */
-
-
     private void Awake()
     {
         socket = GetComponents<XRSocketInteractor>()[0];
         //socket.selectEntered.AddListener(OnSelect);
         //socket.selectExited.AddListener(OnSelectExit);
         socket.onSelectEntered.AddListener(ValidarBandera);
+        socket.onSelectExited.AddListener(LimpiarOutline);
 
     }
 
-    //private void OnSelect(SelectEnterEventArgs arg0) => ValidarBandera(arg0.interactable);
-
-
-    //private void OnSelectExit(SelectExitEventArgs arg0) => Invoke(nameof(AttemptReturnHome), resetDelayTime);
-
     public void ValidarBandera(XRBaseInteractable interactable)
     {
-        if (interactable.gameObject.name.Equals("Bola Azul"))
+        Outline outline = interactable.GetComponent<Outline>();
+        outline.OutlineWidth = 10f;
+        if (interactable.gameObject.name.Equals("Cubo Azul"))
         {
-            Debug.Log("BOLA AZUL AGARRADA!!!!!!!");
+            outline.OutlineColor = Color.green;
         } else
         {
-            socket.recycleDelayTime = 1f;
-            interactable.gameObject.GetComponent<ReturnToHome>().ReturnHome();
+            outline.OutlineColor = Color.red;
         }
+    }
+
+    public void LimpiarOutline(XRBaseInteractable interactable)
+    {
+        Outline outline = interactable.GetComponent<Outline>();
+        outline.OutlineWidth = 0f;
     }
 }
