b47d0e8cf9ad870b1e9b8bbf9c1901f2c73373ef
DBelloso <102482533+DBelloso@users.noreply.github.com>
2022-05-24 20:53:06 +0200

    GestorPuntuacionesEntreEscenas (#26)
    - Script que comprueba la información del nivel
    - Ventana de evento que muestra la información del nivel jugado
    - Clase de atributos estáticos ValoresNivel (Para comunicar información entre escenas, como la dificultad, exitos, fallos, maximos)
    - Cambio de script de cambio de escena (LoadMenu, que carga el menú, y LoadScene, que carga siempre Escena con Mapa y le da una dificultad al atributo dificultad estático)

---
+++ Assets/Scripts/AjustesNivel/ValoresNivel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValoresNivel : MonoBehaviour
+{
+    public static string DIFICULTAD;
+    public static int FALLOS = 5;
+    public static int EXITOS;
+    public static int MAX_EXITOS = 100;
+    public static int MAX_FALLOS;
+}
