2ef773093fa47a4c52fdc3ad929e5ba9c84d9f73
DBelloso <dbdgmaster@gmail.com>
2022-05-26 22:41:02 +0200

    CambioValores
    Los valores del nivel ahora se setean en el menu inicial y no al llegar a la escena con mapa

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -8,11 +8,35 @@ public class ChangeScene : MonoBehaviour
     public void LoadScene(string dificultad)
     {
         ValoresNivel.DIFICULTAD = dificultad;
+        switch (ValoresNivel.DIFICULTAD)
+        {
+            case "Hard":
+                changeValues(10, 3);
+                break;
+
+            case "Easy":
+                changeValues(5, 7);
+                break;
+
+            default:
+
+                break;
+
+        }
         SceneManager.LoadScene("EscenaConMapa");
     }
 
     public static void LoadMenu()
     {
         SceneManager.LoadScene("MenuInicio");
+    }
+
+    private void changeValues(int max_exitos, int fallos)
+    {
+        ValoresNivel.MAX_EXITOS = max_exitos;
+        ValoresNivel.FALLOS = fallos;
+        ValoresNivel.MAX_FALLOS = fallos;
+        ValoresNivel.EXITOS = 0;
+        
     }
 }
